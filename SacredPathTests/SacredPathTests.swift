02d6bb76d87143f1dcc4072c4a7e2d482d72d858
anand ulle <anandulle@gmail.com>
2025-08-16 09:12:54 +0530

    Initial Commit

---
+++ SacredPathTests/SacredPathTests.swift
@@ -0,0 +1,17 @@
+//
+//  SacredPathTests.swift
+//  SacredPathTests
+//
+//  Created by Anand Ulle on 16/08/25.
+//
+
+import Testing
+@testable import SacredPath
+
+struct SacredPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
